32265f8c71e36218592906f9a090ece70549d98c
William Johannes Saaby Porsmose <will5606@edu.campusvejle.dk>
2021-08-11 09:41:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EX02Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Karsten";
+            string efternavn = "Hansen";
+            int alder = 35;
+
+            Console.WriteLine(fornavn);
+            Console.WriteLine(efternavn);
+            Console.WriteLine(alder);
+
+            Console.WriteLine($"fornavn: {fornavn} efternavn: {efternavn} og alder: {alder}.");
+
+
+            string titel = "Lord Of The Rings - The Two Towers";
+            long isbn = 9780007117116;
+            int sider = 450;
+            double pris = 249.95;
+            Console.WriteLine();
+            Console.WriteLine(titel);
+            Console.WriteLine(isbn);
+            Console.WriteLine(sider);
+            Console.WriteLine(pris);
+
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+
+            Console.WriteLine();
+            Console.WriteLine("Maks og mindste int værdier");
+            Console.WriteLine(maxInt);
+            Console.WriteLine(minInt);
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
